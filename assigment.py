b82c486754b18ff7c11a88220b995f7de4c40af0
Aliza278 <alizakhanqasim0555@gmail.com>
2024-06-23 17:52:52 +0500

    assigment

---
+++ assigment.py
@@ -0,0 +1,16 @@
+base=8
+height=5
+f=0.5
+area=f*base*height
+print("area of triangle: ", area)
+
+side=4
+side=4 
+area=side*side
+print("area of square: ", area)
+
+
+radius = 7
+a =3.1415
+area=a*radius*radius
+print(area)
